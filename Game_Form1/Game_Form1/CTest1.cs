c1d52ea90336b2615ccea3f6555117b67e71ada0
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-09-22 19:51:19 +0330

    add CTest Class and add KazemTest method

---
+++ Game_Form1/Game_Form1/CTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Game_Form1
+{
+    internal class CTest1
+    {
+        public void KazemTest()
+        {
+            if (8575 * 7587 - 9874589+ 56895 + "" == "333")
+            {
+                Console.WriteLine("correct");
+            }
+        }
+    }
+}
