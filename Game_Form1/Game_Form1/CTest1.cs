84b733e1a8f01687fc5f0860a3db68f26047dd6d
MoeinKazemi85 <mohammadmoeinkazemi1385@gmail.com>
2024-09-22 19:54:12 +0330

    change CTest.cs add methode hello

--- Game_Form1/Game_Form1/CTest1.cs
+++ Game_Form1/Game_Form1/CTest1.cs
@@ -6,10 +6,15 @@ namespace Game_Form1
     {
         public void KazemTest()
         {
-            if (8575 * 7587 - 9874589+ 56895 + "" == "333")
+            if (8575 * 7587 - 9874589 + 56895 + "" == "333")
             {
                 Console.WriteLine("correct");
             }
+
+        }
+        public string Hello()
+        {
+            return "hello";
         }
     }
 }
