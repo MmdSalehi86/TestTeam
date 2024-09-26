309c3317bceea840397154b113a62d7024c43305
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-09-26 03:35:33 +0330

    fix

--- Game_Form1/Game_Form1/Program.cs
+++ Game_Form1/Game_Form1/Program.cs
@@ -19,7 +19,7 @@ namespace Game_Form1
 
             using (ExcelPackage excel = new ExcelPackage())
             {
-                excel.Workbook.Worksheets.Add("WorkSheet1");
+                excel.Workbook.Worksheets.Add("WorkSheet2");
                 excel.SaveAs("TestEpplus1.xlsx");
             }
 
