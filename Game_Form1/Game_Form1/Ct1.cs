05ada7a386406207b18cff22ca7f12d549ef12d6
MoeinKazemi85 <mohammadmoeinkazemi1385@gmail.com>
2024-10-18 18:27:08 +0330

    ct1

---
+++ Game_Form1/Game_Form1/Ct1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Form1
+{
+    internal class Ct1
+    {
+        public int MyProperty { get; set; }
+    }
+}
